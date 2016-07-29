1e55702faaaf5d14bd17426a5b057591eb4c21c2
jakeykaso <kasoarcat@gmail.com>
2016-07-29 23:15:48 +0800

    setting

--- deployThink.sh
+++ deployThink.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Set Bluemix API Endpoint. You will need this change this if your account use differnt default api endpoint
-cf login -a https://api.ng.bluemix.net
+cf login
 
 # create service
 cf create-service cloudantNoSQLDB Shared Humix-Cloudant-Service
 cf create-service conversation standard Humix-Conversation-Service
 cf create-service speech_to_text standard Humix-Speech-Service
-#cf create-service text_to_speech standard Humix-TTS-Service
+cf create-service text_to_speech standard Humix-TTS-Service
 
 # deploy application
 
